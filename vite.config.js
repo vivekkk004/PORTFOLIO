d7c612284ba88c2acfb004e0e511c38baea7e1f4
Vivek Muthe <vivekmuthe4@gmail.com>
2025-08-03 22:15:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+
+// https://vite.dev/config/
+
 export default defineConfig({
-  base: '/<PORTFOLIO>/',
-  plugins: [react()],
+
+  plugins: [react()],
+
 })
